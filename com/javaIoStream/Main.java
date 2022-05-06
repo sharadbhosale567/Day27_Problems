c1e02337859ff5672e96e5fa941f14d3d5611a96
sharad <sharadbhosale567@gmail.com>
2022-05-06 14:52:49 +0530

    Create multiple contacts

--- com/javaIoStream/Main.java
+++ com/javaIoStream/Main.java
@@ -1,17 +1,2 @@
 package com.javaIoStream;
 
-public class Main {
-    public static void main(String[] args) {
-
-        PersonDetails person = new PersonDetails();
-        person.firstName("Sharad");
-        person.lastName("Bhosale");
-        person.Address("At post nashik");
-        person.City("Nashik");
-        person.State("Maharashtra");
-        person.ZipCode(420000);
-        person.PhoneNumber("7300000000");
-        person.Email("sgb@gmail.com");
-        System.out.println(person.toString());
-    }
-}
--- com/javaIoStream/PersonDetails.java
+++ com/javaIoStream/PersonDetails.java
@@ -6,53 +6,90 @@ public class PersonDetails {
     private String address;
     private String city;
     private String state;
-    private int zipCode;
+    private String zipCode;
     private String phoneNumber;
     private String email;
 
-    public void firstName(String firstName) {
+    public PersonDetails(String firstName, String lastName, String address, String city, String state,
+                         String zipCode, String phoneNumber,  String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zipCode = zipCode;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
-    public void lastName(String lastName) {
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
-    public void Address(String address) {
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
         this.address = address;
     }
 
-    public void City(String city) {
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
         this.city = city;
     }
 
-    public void State(String state) {
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
         this.state = state;
     }
 
-    public void ZipCode(int zipCode) {
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
         this.zipCode = zipCode;
     }
 
-    public void PhoneNumber(String phoneNumber) {
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
 
-    public void Email(String email) {
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
         this.email = email;
     }
 
     @Override
     public String toString() {
-        return "PersonDetails{" +
-                "firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", address='" + address + '\'' +
-                ", city='" + city + '\'' +
-                ", state='" + state + '\'' +
-                ", zipCode=" + zipCode +
-                ", phoneNumber='" + phoneNumber + '\'' +
-                ", email='" + email + '\'' +
-                '}';
+        return "Data [firstName=" + firstName + ", lastName=" + lastName + ", address=" + address + ", city=" + city
+                + ", state=" + state + ", zipCode=" + zipCode + ", phoneNumber=" + phoneNumber + ", email=" + email
+                + "]";
     }
 }
