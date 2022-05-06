38719d846f39d607b4a0b9a2def8492029478429
sharad <sharadbhosale567@gmail.com>
2022-05-06 14:18:41 +0530

    Create contacts in a address book

--- com/javaIoStream/Main.java
+++ com/javaIoStream/Main.java
@@ -2,6 +2,16 @@ package com.javaIoStream;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to the java io files problems");
+
+        PersonDetails person = new PersonDetails();
+        person.firstName("Sharad");
+        person.lastName("Bhosale");
+        person.Address("At post nashik");
+        person.City("Nashik");
+        person.State("Maharashtra");
+        person.ZipCode(420000);
+        person.PhoneNumber("7300000000");
+        person.Email("sgb@gmail.com");
+        System.out.println(person.toString());
     }
 }
